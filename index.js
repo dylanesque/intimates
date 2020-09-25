a2a91b31150c519e3dc938098ce602f504364d6d
Michael Caveney <dylanesque27@gmail.com>
2020-09-25 16:00:30 -0400

    Pushing first prod version

---
+++ index.js
@@ -0,0 +1,12 @@
+const slider = tns({
+  center: true,
+  controlsContainer: document.querySelector('.controls'),
+  nav: false,
+  container: '.my-slider',
+  items: 1,
+  slideBy: 'page',
+  mouseDrag: true,
+  swipeAngle: false,
+  speed: 400,
+  gutter: 20,
+});
